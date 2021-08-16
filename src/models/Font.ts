a4cdaf023a55bbc88a3426e79af3e71aa03fc1eb
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-08-16 09:11:30 -0500

    added uniqueness to fonts

--- src/models/Font.ts
+++ src/models/Font.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from "mongoose";
 import { IFont } from "./interfaces/fonts";
 
-
 const fontSchema = new Schema<IFont>({
-	family:String, category:String
-})
+  family: { type: String, require: true, unique: true },
+  category: String,
+});
 
-export default model<IFont>('Font', fontSchema)
+export default model<IFont>("Font", fontSchema);
