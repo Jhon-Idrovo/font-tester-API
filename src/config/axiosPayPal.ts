f24e7404c1413871687fd2fe03b8c38b421907e1
Jhon Idrovo <jhonidrovo3@gmail.com>
2021-08-30 18:16:14 -0500

    D

--- src/config/axiosPayPal.ts
+++ src/config/axiosPayPal.ts
@@ -5,7 +5,7 @@ const axiosPayPal = axios.create({
   baseURL: PAYPAL_API_URL,
   headers: {
     //https://developer.paypal.com/docs/platforms/get-started/
-    Authorization: `Basic ${"QVRpRWdPVDZSc1IyZElVekFNNEpnOGJiS292enRjRVNubFlfdTY3dDd1MlRXUUczaEFWZXNfcjRYNjNTNmticWxqaGhOdGxySzF2dFNRelg6RU9oRUdXRW9ROEJsbnA0YUR5MGF2RUd2NjV0WHo0Nmo1aGU2TEVyTkJ1d2llVWxKTzVHSlJKWHMxdmpiMzRWUnFWdlc3ZGJPTC1sR3I1NDM="}`,
+    Authorization: `Basic ${"QVZXLUxUYzFTWWp4cWZheDNpZGdna2dYR3g2TEs5Sl9TaWVOWk1QUFp4aFFacDFUTTNuZnNJcTg5M2p1Um1LVE1MTk5CRGpxX2J4Tzl3SUk6RU1UZjNGMXNTU19JY2hsODdRQ2RNU3hnSERVZmk5U1B2MTQ2Q21raXdGTkp3Snh4emFFa2RyVlRHQVdndjA2VGIxeTV2SHMyTUtDa3Bkdnk="}`,
     "Content-Type": "application/json",
   },
 });
